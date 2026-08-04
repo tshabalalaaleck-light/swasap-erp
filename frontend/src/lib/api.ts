01182e066ddf1ebcb7ab102f58ca66e2c09ed895
tshabalalaaleck-light <tshabalalaaleck@gmail.com>
2026-08-05 01:06:36 +0200

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,17 +1,16 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
-  baseURL: API_URL,
+  baseURL: API_URL, // https://swasap-erp-backend-q5gw.onrender.com
   headers: {
-    "Content-Type": "application/json"
+    'Content-Type': 'application/json'
   }
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token && config.headers) {
+  if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
